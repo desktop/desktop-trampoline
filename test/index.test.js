8c66c8abde3a33748881099bdc0e59757248ad79
Markus Olsson <j.markus.olsson@gmail.com>
2020-09-25 14:12:31 +0200

    :art: Cleanup

--- test/index.test.js
+++ test/index.test.js
@@ -1,38 +1,20 @@
 const { stat, access } = require("fs").promises;
 const { constants } = require("fs");
-const { spawn, exec } = require("child_process");
+const { execFile } = require("child_process");
+const { promisify } = require("util");
 const { getAskPassTrampolinePath } = require("../index");
 
 const trampolinePath = getAskPassTrampolinePath();
+const run = promisify(execFile);
 
-function run(args, options) {
-  return new Promise((resolve, reject) => {
-    const cp = spawn(trampolinePath, args, options);
-
-    let stdout = "";
-    let stderr = "";
-
-    cp.stdout.setEncoding("utf8").on("data", (chunk) => (stdout += chunk));
-    cp.stderr.setEncoding("utf8").on("data", (chunk) => (stderr += chunk));
-
-    cp.on("exit", (code) => {
-      resolve({ code, stdout, stderr });
-    });
-  });
-}
-
-test("trampoline exists and is a regular file", async () => {
-  const s = await stat(trampolinePath);
-  expect(s.isFile()).toBe(true);
-});
+test("trampoline exists and is a regular file", async () =>
+  expect((await stat(trampolinePath)).isFile()).toBe(true));
 
 test("can be executed by current process", () =>
   access(trampolinePath, constants.X_OK));
 
-test("fails when required environment variables are missing", async () => {
-  const { code } = await run(["Username"]);
-  expect(code).not.toBe(0);
-});
+test("fails when required environment variables are missing", () =>
+  expect(run(trampolinePath, ["Username"])).rejects.toThrow());
 
 test("smoke test", async () => {
   const echoPath =
@@ -40,9 +22,11 @@ test("smoke test", async () => {
       ? "C:\\Program Files\\Git\\usr\\bin\\echo.exe"
       : "/bin/echo";
 
-  const { code, stdout } = await run(["Username"], {
-    env: { DESKTOP_PATH: echoPath, DESKTOP_ASKPASS_SCRIPT: "scriptPath" },
+  const env = { DESKTOP_PATH: echoPath, DESKTOP_ASKPASS_SCRIPT: "scriptPath" };
+  const opts = { env };
+
+  expect(run(trampolinePath, ["Username"], opts)).resolves.toEqual({
+    stdout: "scriptPath Username\n",
+    stderr: "",
   });
-  expect(code).toBe(0);
-  expect(stdout).toEqual("scriptPath Username\n");
 });
