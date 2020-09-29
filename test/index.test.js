87d6769cc93a10be61b4a98fb0e4db2b68578f3f
Markus Olsson <j.markus.olsson@gmail.com>
2020-09-29 12:28:28 +0200

    Prettify

--- test/index.test.js
+++ test/index.test.js
@@ -1,37 +1,37 @@
-const { stat, access } = require("fs").promises;
-const { constants } = require("fs");
-const { execFile } = require("child_process");
-const { promisify } = require("util");
-const { getAskPassTrampolinePath } = require("../index");
+const { stat, access } = require('fs').promises
+const { constants } = require('fs')
+const { execFile } = require('child_process')
+const { promisify } = require('util')
+const { getAskPassTrampolinePath } = require('../index')
 
-const trampolinePath = getAskPassTrampolinePath();
-const run = promisify(execFile);
+const trampolinePath = getAskPassTrampolinePath()
+const run = promisify(execFile)
 
-describe("ask-pass-trampoline", () => {
-  it("exists and is a regular file", async () =>
-    expect((await stat(trampolinePath)).isFile()).toBe(true));
+describe('ask-pass-trampoline', () => {
+  it('exists and is a regular file', async () =>
+    expect((await stat(trampolinePath)).isFile()).toBe(true))
 
-  it("can be executed by current process", () =>
-    access(trampolinePath, constants.X_OK));
+  it('can be executed by current process', () =>
+    access(trampolinePath, constants.X_OK))
 
-  it("fails when required environment variables are missing", () =>
-    expect(run(trampolinePath, ["Username"])).rejects.toThrow());
+  it('fails when required environment variables are missing', () =>
+    expect(run(trampolinePath, ['Username'])).rejects.toThrow())
 
-  it("forwards arguments correctly", async () => {
+  it('forwards arguments correctly', async () => {
     const echoPath =
-      process.platform === "win32"
-        ? "C:\\Program Files\\Git\\usr\\bin\\echo.exe"
-        : "/bin/echo";
+      process.platform === 'win32'
+        ? 'C:\\Program Files\\Git\\usr\\bin\\echo.exe'
+        : '/bin/echo'
 
     const env = {
       DESKTOP_PATH: echoPath,
-      DESKTOP_ASKPASS_SCRIPT: "foo bar",
-    };
-    const opts = { env };
+      DESKTOP_ASKPASS_SCRIPT: 'foo bar',
+    }
+    const opts = { env }
 
-    expect(run(trampolinePath, ["baz"], opts)).resolves.toEqual({
-      stdout: "foo bar baz\n",
-      stderr: "",
-    });
-  });
-});
+    expect(run(trampolinePath, ['baz'], opts)).resolves.toEqual({
+      stdout: 'foo bar baz\n',
+      stderr: '',
+    })
+  })
+})
