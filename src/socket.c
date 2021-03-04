ff581e8b446ff340d4854b691d409338f2377450
Sergio Padrino <sergio.padrino@gmail.com>
2021-03-04 10:52:04 +0100

    Get description for Winsock errors

--- src/socket.c
+++ src/socket.c
@@ -65,7 +65,13 @@ void printSocketError(char *fmt, ...)
   va_end(argptr);
 
 #ifdef WINDOWS
-  fprintf(stderr, "%s: %ld\n", formatted_string, WSAGetLastError());
+  wchar_t *errorDescription = NULL;
+  FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, 
+                 NULL, WSAGetLastError(),
+                 MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                 (LPWSTR)&errorDescription, 0, NULL);
+
+  fprintf(stderr, "%s (%ld): %S\n", formatted_string, WSAGetLastError(), errorDescription);
 #else
   fprintf(stderr, "%s (%d): %s\n", formatted_string, errno, strerror(errno));
 #endif
